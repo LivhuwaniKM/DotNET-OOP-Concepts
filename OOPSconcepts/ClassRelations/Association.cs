2df1edffff6b5d385901019aec840f8380493d42
LivhuwaniKM <lkmasindi1208@gmail.com>
2024-03-28 10:29:20 +0200

    directory refactor

---
+++ OOPSconcepts/ClassRelations/Association.cs
@@ -0,0 +1,55 @@
+﻿namespace OOPSconcepts.Relations
+{
+    public class Association
+    {
+        public string Main()
+        {
+            Car car = GetCar();
+            var carDescription = CreateDisplayForCar(car);
+
+            var output =
+                $"==============================\n" +
+                $"Class Relations: Association\n" +
+                $"==============================\n" +
+                $"\n{carDescription}";
+
+            return output;
+        }
+
+        public Car GetCar()
+        {
+            return new()
+            {
+                Id = 1,
+                Model = "300CE",
+                Make = "Mercedes Benz",
+                Code = "W124",
+                Year = DateTime.Now,
+                FuelType = "Petrol"
+            };
+        }
+
+        public string CreateDisplayForCar(Car car)
+        {
+            var displayOutput = (
+                $"Car Properties\n" +
+                $"------------------------------\n" +
+                $"Model: {car.Model}\n" +
+                $"Make: {car.Make}\n" +
+                $"Year: {car.Year}\n" +
+                $"Fuel: {car.FuelType}"
+            );
+            return displayOutput;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public DateTime Year { get; set; }
+        public string FuelType { get; set; } = string.Empty;
+    }
+}
---
+++ OOPSconcepts/ClassRelations/Composition.cs
@@ -0,0 +1,81 @@
+﻿namespace OOPSconcepts.Relations
+{
+    public class Composition
+    {
+        public string Main()
+        {
+            var vehicle = GetVehicle();
+            var description = GetVehicleAndDescription();
+
+            var displayOutput = (
+                $"==============================\n" +
+                $"Class Relations: Composition\n" +
+                $"==============================\n\n" +
+                $"Vehicle\n" +
+                $"------------------------------\n" +
+                $"Vin: {vehicle.Vin}\n" +
+                $"Model: {vehicle.Model}\n" +
+                $"Make: {vehicle.Make}\n" +
+                $"Code: {vehicle.Code}\n" +
+                $"Year: {vehicle.Year}\n" +
+                $"Fuel: {vehicle.FuelType}\n" +
+                $"\nVehicle Details\n" +
+                $"------------------------------\n" +
+                $"Color: {description.Description.Color}\n" +
+                $"Tyre Size: {description.Description.TyreSize}\n" +
+                $"Transmission: {description.Description.Transmission}\n" +
+                $"Turbo: {description.Description.Turbo}"
+            );
+            return displayOutput;
+        }
+
+        public Vehicle GetVehicle()
+        {
+            return new()
+            {
+                Vin = "ABC123456789",
+                Model = "200E",
+                Make = "Mecredes Benz",
+                Code = "W201",
+                Year = DateTime.Now,
+                FuelType = "Petrol"
+            };
+        }
+
+        public Vehicle GetVehicleAndDescription()
+        {
+            var vehicle = GetVehicle();
+
+            vehicle.Description = new()
+            {
+                Vin = vehicle.Vin,
+                Color = "Candy apple blue",
+                TyreSize = "145/50 R15",
+                Transmission = "Manual",
+                Turbo = true
+            };
+            return vehicle;
+        }
+    }
+
+    public class Vehicle
+    {
+        public string Vin { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public DateTime Year { get; set; }
+        public string FuelType { get; set; } = string.Empty;
+
+        public Description? Description;
+    }
+
+    public class Description
+    {
+        public string Vin { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string TyreSize { get; set; } = string.Empty;
+        public string Transmission { get; set; } = string.Empty;
+        public bool Turbo { get; set; } = false;
+    }
+}
